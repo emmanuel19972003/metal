4d60abdc7a59cc2059c9f6a8d4746bf537abbbb0
Emma <emmauel1997@mail.com>
2024-03-01 10:25:39 -0500

    cahnge entry point

---
+++ teaPot/teaPodViewController.swift
@@ -0,0 +1,17 @@
+//
+//  teaPodViewController.swift
+//  tringuloMetal
+//
+//  Created by Emmanuel Zambrano on 1/03/24.
+//
+
+import UIKit
+
+class teaPodViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+}
